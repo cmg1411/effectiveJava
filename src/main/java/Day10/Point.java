96f8b54fbbff2d520b1eb9895723d38c28a73924
tomas <cmg14111@gmail.com>
2021-01-17 19:18:30 +0900

    feat: Day10 실습코드

---
+++ src/main/java/Day10/Point.java
@@ -0,0 +1,20 @@
+package Day10;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Point)) {
+            return false;
+        }
+        Point p = (Point) o;
+        return p.x == x && p.y == y;
+    }
+}
---
+++ src/main/java/Day10/null_아님.java
@@ -0,0 +1,11 @@
+package Day10;
+
+/**
+ * 모든 객체는 null 과 equals 가 true 이면 안된다.
+ *
+ * 명시적으로 == null 을 검사하기보다는
+ * 묵시적으로 instanceOf 를 이용하여 검사하도록 한다. (ClassCastException)
+ */
+public class null_아님 {
+
+}
