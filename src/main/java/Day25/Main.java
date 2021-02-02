ce017dbd4c7bfe4ddfec85621aad4324e8014254
tomas <cmg1411@naver.com>
2021-02-02 23:58:28 +0900

    feat: item26 실습코드

--- src/main/java/Day25/Main.java
+++ src/main/java/Day25/Main.java
@@ -1,7 +1,64 @@
 package Day25;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(TopLevel.NAME + Toplevel2.NAME);
+import java.util.ArrayList;
+import java.util.List;
+
+class LandAnimal { public void crying() { System.out.println("육지동물"); } }
+
+class Cat extends LandAnimal { public void crying() { System.out.println("냐옹냐옹"); } }
+
+class Dog extends LandAnimal { public void crying() { System.out.println("멍멍"); } }
+
+class Sparrow { public void crying() { System.out.println("짹짹"); } }
+
+
+
+class AnimalList<T> {
+
+    ArrayList<T> al = new ArrayList<T>();
+
+
+
+    public static void cryingAnimalList(AnimalList<? extends LandAnimal> al) {
+
+        LandAnimal la = al.get(0);
+
+        la.crying();
+
     }
+
+
+
+    void add(T animal) { al.add(animal); }
+
+    T get(int index) { return al.get(index); }
+
+    boolean remove(T animal) { return al.remove(animal); }
+
+    int size() { return al.size(); }
+
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AnimalList<Cat> catList = new AnimalList<Cat>();
+
+        catList.add(new Cat());
+
+        AnimalList<Dog> dogList = new AnimalList<Dog>();
+
+        dogList.add(new Dog());
+
+
+
+        AnimalList.cryingAnimalList(catList);
+
+        AnimalList.cryingAnimalList(dogList);
+
+    }
+
 }
---
+++ src/main/java/Day26/ObjectGeneric.java
@@ -0,0 +1,17 @@
+package Day26;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectGeneric {
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        // unsafeAdd(strings, Integer.valueOf(42));
+        String s = strings.get(0);
+    }
+
+    private static void unsafeAdd(List<Object> strings, Object value) {
+        strings.add(value);
+    }
+
+}
