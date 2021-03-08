26622867eb3207f346bf9482e32f7fa77efafe79
tomas <cmg1411@naver.com>
2021-03-09 00:56:50 +0900

    item51 실습코드

---
+++ src/main/java/Chapter8/Day51/CardDeck.java
@@ -0,0 +1,18 @@
+package Chapter8.Day51;
+
+public class CardDeck {
+
+    static class Card {
+        private String rank;
+        private String suit;
+
+        public Card(String rank, String suit) {
+            this.rank = rank;
+            this.suit = suit;
+        }
+    }
+
+    public void shuffle(Card deck) {
+
+    }
+}
